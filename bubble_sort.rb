8d775babbcce8d48857193bbe813246464eac8ed
RunTheLi <lanmarock@gmail.com>
2024-06-30 22:24:43 +0700

    Add Code

---
+++ bubble_sort.rb
@@ -0,0 +1,28 @@
+#> bubble_sort([4,3,78,2,0,2])
+#=> [0,2,2,3,4,78]
+
+#Build a method #bubble_sort that takes an array and returns a sorted array.
+#It must use the bubble sort methodology (using #sort would be pretty pointless, wouldn’t it?).
+
+def bubble_sort(array)
+    n = array.length
+    return array if n <= 1
+
+    loop do
+        swapped = false
+        (n-1).times do |i|
+            if array[i] > array[i + 1]
+                array[i], array[i + 1] = array[i + 1], array[i]
+                swapped = true 
+            end
+        end
+
+            break unless swapped
+        end
+
+          array
+        end 
+
+    arr = [5, 3, 8, 4, 2]
+    sorted_arr = bubble_sort(arr)
+    puts "Sorted array: #{sorted_arr.inspect}"
